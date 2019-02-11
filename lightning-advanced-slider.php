c604cff495dab10ce34d4180ba152b512c1aea3f
kurudrive <kurudrive@gmail.com>
2019-02-11 23:47:21 +0900

    1st commit

---
+++ lightning-advanced-slider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:     Lightning Advanced Slider
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     lightning-advanced-slider
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Lightning_Advanced_Slider
+ */
+
+// Your code starts here.
+
+define( 'VK_SLIDER_DIR', plugin_dir_path( __FILE__ ) );
+
+require_once( VK_SLIDER_DIR . 'inc/term-color-config.php' );
