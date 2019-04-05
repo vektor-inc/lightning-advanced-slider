08f06df16841544c7d2f6ed4916417405cf76f8a
kurudrive <kurudrive@gmail.com>
2019-04-05 13:02:00 +0900

    [ Change version ] 0.1.2

--- lightning-advanced-slider.php
+++ lightning-advanced-slider.php
@@ -7,7 +7,7 @@
  * Author URI:      https://lightning.nagoya/
  * Text Domain:     lightning-advanced-slider
  * Domain Path:     /languages
- * Version:         0.1.1
+ * Version:         0.1.2
  *
  * @package         Lightning_Advanced_Slider
  */
